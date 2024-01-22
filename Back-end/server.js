73360090dbcf102452c64f45b8a73558fad5785e
Ankita Jha <ankitajha@Ankitas-MacBook-Air.local>
2024-01-22 13:52:57 -0500

    Integrated MongoDB with Express app

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,12 +1,23 @@
 // this will be entry point  to my Express application
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.Port || 3000;
+// a MongoDB object modeling tool for Node.js - Mongoose
+const mongoose = require("mongoose");
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
+const mongoDBUri =
+  "mongodb+srv://ankita23:myDB@cluster0.ttzjb.mongodb.net/?retryWrites=true&w=majority";
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
